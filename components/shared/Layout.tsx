50188a4fd3af00790cce8013c0445e5593000644
Julian <julianpalomodeonis@ordersmadesimple.com>
2021-10-24 11:03:34 +0100

    Add global styles and layout component

--- components/shared/Layout.tsx
+++ components/shared/Layout.tsx
@@ -1,8 +1,22 @@
-import { Box } from 'grommet'
+import Head from 'next/head'
 import { FunctionComponent } from 'react'
+import { Wrapper, Main } from '../../styles/styles'
+
+export const siteTitle = 'My personal web site'
 
 const Layout: FunctionComponent = ({ children }) => {
-  return <Box>{children}</Box>
+  return (
+    <Wrapper>
+      <Head>
+        <link rel='icon' href='/favicon.ico' />
+        <meta name='description' content='Info about myself' />
+        <meta name='og:title' content={siteTitle} />
+        <meta name='twitter:card' content='summary_large_image' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <Main>{children}</Main>
+    </Wrapper>
+  )
 }
 
 export default Layout
--- pages/work.tsx
+++ pages/work.tsx
@@ -1,16 +1,21 @@
+import Head from 'next/head'
 import Link from 'next/link'
-import { Fragment, FunctionComponent } from 'react'
+import { FunctionComponent } from 'react'
+import Layout from '../components/shared/Layout'
 
 const WorkPage: FunctionComponent = () => {
   return (
-    <Fragment>
+    <Layout>
+      <Head>
+        <title>My work experience</title>
+      </Head>
       <h1>Work page</h1>
       <h2>
         <Link href='/'>
           <a>Back to home</a>
         </Link>
       </h2>
-    </Fragment>
+    </Layout>
   )
 }
 
